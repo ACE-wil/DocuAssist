3f18d05fdb6e167ec81cfec9ee6fbfb8f1a674e5
ACE-wil <118677338+ACE-wil@users.noreply.github.com>
2024-11-29 18:24:07 +0800

    修复热门商店页面报错
    修复热门商店页面报错

--- pages/store/popular.js
+++ pages/store/popular.js
@@ -91,9 +91,10 @@ export default function PopularApps() {
         </div>
       </div>
       <div className="myTree">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => {
           return (
             <Card
+              key={index}
               hoverable
               style={{
                 width: "33%",
